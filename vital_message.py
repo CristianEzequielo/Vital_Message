9791a84e974eaaa8a489329db3295173e729778d
Cristian <163317720+CristianEzequielo@users.noreply.github.com>
2024-05-22 00:02:47 -0300

    Add files via upload

---
+++ vital_message.py
@@ -0,0 +1,39 @@
+import os
+import random
+import string
+import time
+
+os.system('cls || clear')
+
+print("VITAL MESSAGE"
+      "\n")
+
+while True:
+    D = int(input("HOW DIFFICULT? (4 - 10)"
+                  "\n"))
+    if D >= 4 and D <= 10:
+        break
+    
+M = ""
+
+for i in range(D):
+    M += random.choice(string.ascii_uppercase)
+    
+os.system('cls || clear')
+
+print("SEND THIS MESSAGE:"
+      "\n"
+      "\n", M)
+time.sleep(0.5*D)
+
+os.system('cls || clear')
+
+N = input("")
+
+if N == M:
+    print("MESSAGE CORRECT"
+          "\n" "THE WAR IS OVER")
+else:
+    print("YOU GOT IT WRONG"
+          "\n""YOU SHOULD HAVE SENT:"
+          "\n", M)
